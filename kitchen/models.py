6eec5e6e7fe8f9ee12fbf7f95633f211e3b40b1f
diemonBil <crazyevildocs@gmail.com>
2025-03-13 17:43:14 +0200

    Create models for cafe_kitchen_service

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
+from cafe_kitchen_service import settings
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0)]
+    )
+    class Meta:
+        ordering = ('username', )
+    def __str__(self):
+        return f"{self.username}: ({self.first_name} {self.last_name})"
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True, max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes",
+    )
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        ordering = ('name', )
+    def __str__(self):
+        return f"{self.name} (price: {self.price}, type: {self.dish_type.name})"
