f6cd752011f1602735fa9392d5e1ed37634d6f31
diemonBil <crazyevildocs@gmail.com>
2025-03-16 09:58:11 +0200

    Implement cook list template and add create functionality

--- cafe_kitchen_service/urls.py
+++ cafe_kitchen_service/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from kitchen import views
+
+app_name = "kitchen"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cook-list/', views.CookListView.as_view(), name='cook_list'),
+    path('cook-list/create/', views.CookListCreateView.as_view(), name='cook_create'),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from kitchen.models import Cook
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    template_name = 'kitchen/cook_list.html'
+    context_object_name = 'cook_list'
+
+class CookListCreateView(generic.CreateView):
+    model = Cook
+    fields = "__all__"
+    success_url = reverse_lazy('kitchen:cook_list')
+    template_name = 'kitchen/cook_form.html'
