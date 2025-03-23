811fbc38aae87b01f71b3f84ffc1bc5b412d99e6
diemonBil <crazyevildocs@gmail.com>
2025-03-23 10:42:56 +0200

    preparing to Deploy on Render

--- cafe_kitchen_service/settings/prod.py
+++ cafe_kitchen_service/settings/prod.py
@@ -4,7 +4,11 @@ from .base import *
 DEBUG = False
 
 # Hosts that are allowed to serve this Django project
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database configuration (using SQLite for development)
 DATABASES = {
